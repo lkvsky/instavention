78ca6b78edb0f6d2d2315e4e72b4781d01dcf32e
lkvsky <kyle@lucovsky.com>
2013-02-17 09:48:12 -0800

    updated instagram keys

--- config/initializers/01_secret_keys.rb
+++ config/initializers/01_secret_keys.rb
@@ -3,7 +3,7 @@ CLIENT_ID = "ab44ef77097d40428ecc175f9da1819a"
 
 CLIENT_SECRET = "b5e1d991cbfc435dad046374fd40a25f"
 
-CALLBACK_URL = "http://instavention.herokuapp.com//users/new"
+CALLBACK_URL = "http://instavention.herokuapp.com/users/new"
 
 #### DEV keys
 # CLIENT_ID = "972d39944dc4408bba8dd909f670cb19"
