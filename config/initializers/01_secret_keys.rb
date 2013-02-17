e8e42a6563cbc874507c42f7b6408f195c0424d8
lkvsky <kyle@lucovsky.com>
2013-02-16 23:27:51 -0800

    changed instagram callback url

--- config/initializers/01_secret_keys.rb
+++ config/initializers/01_secret_keys.rb
@@ -2,4 +2,4 @@ CLIENT_ID = "ab44ef77097d40428ecc175f9da1819a"
 
 CLIENT_SECRET = "b5e1d991cbfc435dad046374fd40a25f"
 
-CALLBACK_URL = "http://localhost:3000/users/new"
+CALLBACK_URL = "http://instavention.herokuapp.com//users/new"
